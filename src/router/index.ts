3481cdcd6466bd36b2eab7cdee684aab22cae95d
Milos Bilic <milosrcz@gmail.com>
2023-04-29 20:36:18 +0200

    Added home, login and sign up pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,13 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Login from '@/views/Login.vue'
+import Register from '@/views/Register.vue'
+import Home from '@/views/Home.vue'
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  {path: '/login', component: Login},
+  {path: '/register', component: Register},
+  {path: '/', component: Home}
 ]
 
 const router = createRouter({
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container mt-5 text-center">
+        <h3>Hello guys, have a nice day!</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
