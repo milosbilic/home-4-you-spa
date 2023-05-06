9309178223de6f8d514f85509b464dee8029af54
Milos Bilic <milosrcz@gmail.com>
2023-05-06 15:34:30 +0200

    WIP

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,23 @@
 <template>
     <div class="container mt-5 text-center">
-        <h3>Hello guys, have a nice day!</h3>
+        <h3>Home 4 You: Where Happiness Finds Its Perfect Address</h3>
+        <h3>{{ message }}</h3>
     </div>
 </template>
 
 <script>
+    import { ref } from "vue";
+    
     export default {
-        name: "Home"
+        name: "Home",
+        setup() {
+            const message = ref('You are not logged in');
+
+            onMounted()
+
+            return {
+                message
+            }
+        }
     }
 </script>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,35 +1,69 @@
 <template>
     <main class="form-signin w-100 m-auto">
-        <form>
-            <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+        <form @submit.prevent="submit">
+            <h1 class="h3 mb-3 fw-normal">Sign up to Home 4 You</h1>
 
             <div class="form-floating">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                <label for="floatingInput">Email address</label>
+                <input v-model="data.email" type="email" class="form-control" id="email" placeholder="name@example.com">
+                <label for="email">Email address</label>
             </div>
             <div class="form-floating">
-                <input class="form-control" id="floatingInput" placeholder="name@example.com">
-                <label for="floatingInput">First name</label>
+                <input v-model="data.firstName" class="form-control" placeholder="name@example.com">
+                <label for="firstName">First name</label>
             </div>
             <div class="form-floating">
-                <input class="form-control" id="floatingInput" placeholder="name@example.com">
-                <label for="floatingInput">Last name</label>
+                <input v-model="data.lastName" class="form-control" placeholder="name@example.com">
+                <label for="lastName">Last name</label>
             </div>
             <div class="form-floating">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                <label for="floatingInput">Email address</label>
+                <input v-model="data.phone" class="form-control" placeholder="name@example.com">
+                <label for="phone">Phone</label>
             </div>
             <div class="form-floating">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <input v-model="data.password" type="password" class="form-control" id="floatingPassword"
+                    placeholder="Password">
                 <label for="floatingPassword">Password</label>
             </div>
+            <div class="form-floating">
+                <input v-model="data.repeatedPassword" type="password" class="form-control" id="floatingPassword"
+                    placeholder="Password">
+                <label for="floatingPassword">Repeat password</label>
+            </div>
+
             <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
         </form>
     </main>
 </template>
 
-<script>
-    export default {
-        name: "Register"
+<script lang="ts">
+import { reactive } from "vue";
+import axios from 'axios';
+import { useRouter } from "vue-router";
+export default {
+    name: "Register",
+    setup() {
+        const data = reactive({
+            firstName: '',
+            lastName: '',
+            email:'',
+            phone: '',
+            password: '',
+            repeatedPassword: '',
+            role: 'ROLE_USER'
+        });
+
+        const router = useRouter();
+
+        const submit = async() => {
+            await axios.post("http://localhost:8000/api/users", data);
+
+            await router.push('/login');
+        }
+
+        return {
+            data,
+            submit
+        }
     }
+}
 </script>
